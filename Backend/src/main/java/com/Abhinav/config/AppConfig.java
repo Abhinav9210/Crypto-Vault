82301be13dcdff6d97d27d0190138a0ca19d6e37
abhinav9210 <abhinav83423@gmail.com>
2026-04-09 22:21:57 +0530

    OAuth Bug solved

--- Backend/src/main/java/com/Abhinav/config/AppConfig.java
+++ Backend/src/main/java/com/Abhinav/config/AppConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+//import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -67,40 +67,40 @@ public class AppConfig {
 							})
 //							.authenticationProvider((AuthenticationProvider) authProvider)
 					)
-					.oauth2Login(oauth->{
-						oauth.loginPage("/login/google");
-						oauth.authorizationEndpoint(authorization->
-								authorization.baseUri("/login/oauth2/authorization"));
-						oauth.successHandler(new AuthenticationSuccessHandler() {
-
-							@Override
-							public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-																Authentication authentication) throws IOException, ServletException {
-
-								if(authentication.getPrincipal() instanceof DefaultOAuth2User) {
-									DefaultOAuth2User userDetails = (DefaultOAuth2User) authentication.getPrincipal();
-									String email = userDetails.getAttribute("email");
-									String fullName=userDetails.getAttribute("name");
-									String phone=userDetails.getAttribute("phone");
-									String picture=userDetails.getAttribute("picture");
-									boolean email_verified= Boolean.TRUE.equals(userDetails.getAttribute("email_verified"));
-
-									User user=new User();
-									user.setVerified(email_verified);
-									user.setFullName(fullName);
-									user.setEmail(email);
-									user.setMobile(phone);
-									user.setPicture(picture);
-
-									System.out.println("--------------- " + email+
-											"-------------"+
-											"==========="
-									+"-------"+user);
-								}
-
-							}
-						});
-					})
+//					.oauth2Login(oauth->{
+//						oauth.loginPage("/login/google");
+//						oauth.authorizationEndpoint(authorization->
+//								authorization.baseUri("/login/oauth2/authorization"));
+//						oauth.successHandler(new AuthenticationSuccessHandler() {
+//
+//							@Override
+//							public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+//																Authentication authentication) throws IOException, ServletException {
+//
+//								if(authentication.getPrincipal() instanceof DefaultOAuth2User) {
+//									DefaultOAuth2User userDetails = (DefaultOAuth2User) authentication.getPrincipal();
+//									String email = userDetails.getAttribute("email");
+//									String fullName=userDetails.getAttribute("name");
+//									String phone=userDetails.getAttribute("phone");
+//									String picture=userDetails.getAttribute("picture");
+//									boolean email_verified= Boolean.TRUE.equals(userDetails.getAttribute("email_verified"));
+//
+//									User user=new User();
+//									user.setVerified(email_verified);
+//									user.setFullName(fullName);
+//									user.setEmail(email);
+//									user.setMobile(phone);
+//									user.setPicture(picture);
+//
+//									System.out.println("--------------- " + email+
+//											"-------------"+
+//											"==========="
+//									+"-------"+user);
+//								}
+//
+//							}
+//						});
+//					})
 	                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
 	                .csrf(csrf -> csrf.disable())
 	                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
